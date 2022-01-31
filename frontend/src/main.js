d4ae1551a5a4c47985fe7a219317a11200a10b6f
Dastan Turysbekov <uitlaber@gmail.com>
2022-01-31 15:30:40 +0600

    Fix #1

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,15 @@
 import {
     createApp
 } from 'vue'
-import PrimeVue from 'primevue/config';
-import Card from 'primevue/card';
-import InputText from 'primevue/inputtext';
-import Button from 'primevue/button';
-import ToastService from 'primevue/toastservice';
-import Toast from 'primevue/toast';
-
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import ColumnGroup from 'primevue/columngroup';
+import PrimeVue from 'primevue/config'
+import Card from 'primevue/card'
+import InputText from 'primevue/inputtext'
+import Button from 'primevue/button'
+import ToastService from 'primevue/toastservice'
+import Toast from 'primevue/toast'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import ColumnGroup from 'primevue/columngroup'
 
 
 import router from './router'
@@ -22,11 +21,11 @@ import VueClipboard from 'vue-clipboard2'
 VueClipboard.config.autoSetContainer = true // add this line
 
 
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
 
-window.axios = require('axios');
+window.axios = require('axios')
 
 
 
@@ -51,12 +50,12 @@ app.use(PrimeVue)
 app.use(ToastService)
 app.use(VueClipboard)
 
-app.component('Card', Card);
-app.component('InputText', InputText);
-app.component('Button', Button);
-app.component('Toast', Toast);
-app.component('DataTable', DataTable);
-app.component('Column', Column);
-app.component('ColumnGroup', ColumnGroup);
+app.component('Card', Card)
+app.component('InputText', InputText)
+app.component('Button', Button)
+app.component('Toast', Toast)
+app.component('DataTable', DataTable)
+app.component('Column', Column)
+app.component('ColumnGroup', ColumnGroup)
 
 app.mount('#app')
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -26,9 +26,6 @@ const routes = [{
              middleware: [guest],
          },
     },
-
-    // Always leave this as last one,
-    // but you can also remove it
     {
         path: '/:catchAll(.*)*',
         component: () => import('../pages/Error404.vue')
